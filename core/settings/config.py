77ea0c0414d96930dbca76bc36c8c3b990664c79
Аркадий <arkgaranin@gmail.com>
2026-04-03 21:23:51 +0300

    Добавил файлы endpoints, config. Добавлены новые функции в апи клиент: ping, auth, get_booking_by_id

---
+++ core/settings/config.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Users(Enum):
+    USERNAME = "admin"
+    PASSWORD = "password123"
+
+class Timeouts(Enum):
+    TIMEOUT = 5
+
+class BookingId(Enum):
+    BOOKING1 = 1
